bc51694b8d0c9ff0bbdb3cbcba9601024f2a9850
SamuelFSNunes <samucasantana2003@gmail.com>
2024-03-28 20:16:12 -0300

    feat: add listview
    added GET logic to display all classes

---
+++ api/views/ListView.py
@@ -0,0 +1,17 @@
+from django.views import View
+from django.shortcuts import render, redirect
+from api.repositories import ClassRepository
+from api.serializers import ClassSerializer, ClassEntity
+from datetime import datetime
+
+class ClassList(View):
+    def get(self, request):
+        repository = ClassRepository(collectionName='classes')
+        classes = list(repository.getAll())
+        serializer = ClassSerializer(data=classes, many=True)
+        if (serializer.is_valid()):
+            modelclass = serializer.save()
+            print(serializer.data)
+        else:
+            print(serializer.errors)
+        return render(request, "home.html", {"class":modelclass})
